79c6f64a0091df5b729110a1b1cdc9e1642b4468
soogoori <soobin0209@naver.com>
2025-02-16 19:13:04 +0900

    ✨ feat : main 메서드 실행 기능

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,13 +1,26 @@
+import stringCalculator.calculate.Calculate;
 import stringCalculator.utils.StringParser;
 import stringCalculator.view.InputView;
+import stringCalculator.view.OutputView;
+
+import java.util.List;
+import java.util.Map;
 
 public class Application {
     public static void main(String[] args){
 
         // 계산식 입력받기
-        String value = InputView.inputString();
+        String[] value = InputView.inputString();
 
+        Map<String, List<String>> resultMap = StringParser.parseString(value);
         // 계산식 파싱
-        StringParser.parseString(value);
+        List<String> numbers =  resultMap.get("numbers");
+        List<String> operators = resultMap.get("operators");
+
+        // 계산하기
+        int result = Calculate.calculate(numbers, operators);
+
+        // 계산결과 출력하기
+        OutputView.printResultMessage(result);
     }
 }
