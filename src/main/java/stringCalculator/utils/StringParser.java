cdce43b439dbe2dd4157cf8e33b004dcd7ba3902
soogoori <soobin0209@naver.com>
2025-02-16 19:12:38 +0900

    ✨ feat : 문자열 파싱 기능 수정 및 추가

--- src/main/java/stringCalculator/utils/StringParser.java
+++ src/main/java/stringCalculator/utils/StringParser.java
@@ -1,12 +1,32 @@
 package stringCalculator.utils;
 
+import stringCalculator.exception.ExceptionHandler;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class StringParser {
 
-    public static List<Integer> parseString(String value) {
-        String result = value.replace(" ", "");
+    public static Map<String, List<String>> parseString (String[] value) {
+        List<String> numbers = new ArrayList<>();
+        List<String> operators = new ArrayList<>();
 
+        for(String str : value) {
+            if(str.matches("-?\\d+")) {
+                numbers.add(str);
+            } else if (str.matches("[+\\-*/]")) {
+                operators.add(str);
+            }
+        }
+
+        ExceptionHandler.validateOperators(numbers, operators);
+
+        Map<String, List<String>> resultMap = new HashMap<>();
+        resultMap.put("numbers", numbers);
+        resultMap.put("operators", operators);
+
+        return resultMap;
     }
-
 }
