e9cbeddb6bb1dbd210a1ed0ffc2b1b8d68396a7d
soogoori <soobin0209@naver.com>
2025-02-22 16:31:07 +0900

    ✨ feat : 출력 기능 구현

---
+++ src/main/java/baseball/view/OutputView.java
@@ -0,0 +1,35 @@
+package baseball.view;
+
+import java.util.Map;
+
+public class OutputView {
+
+    public static void printStartMessage() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+    public static void printInputMessage() {
+        System.out.print("숫자를 입력해 주세요 : ");
+    }
+
+    public static void printCorrectMessage() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+
+    public static String printResult(Map<String, Integer> strike, Map<String, Integer> ball) {
+
+        int strikes = strike.get("스트라이크");
+        int balls = ball.get("볼");
+
+        if(strikes > 0 && balls > 0 ){
+            String ballResult = balls + "볼";
+            String strikeResult = strikes + "스트라이크";
+
+            return ballResult + " " + strikeResult;
+        }
+
+        if(balls > 0 && strikes == 0) return balls + "볼";
+        if(strikes > 0 && balls == 0) return strikes + "스트라이크";
+
+        return "nothing";
+    }
+}
